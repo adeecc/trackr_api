f301925c648bda28d93a8300a847ef6f2d85217b
Aditya Chopra <adi.chopra108@gmail.com>
2020-11-01 01:30:07 +0530

    Added .ngrok.io to allowed hosts for tunneling

--- api/settings.py
+++ api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'c5@^y0+hdlnsn2#!-&f=_b@i563(i@=-b$v3bhhjyq&-$q^++_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".ngrok.io"]
 
 AUTH_USER_MODEL = 'authentication.User'
 
