eae7a37729896f3ee51717b354bbc370830212a6
Aditya Chopra <adi.chopra108@gmail.com>
2020-11-22 02:31:47 +0530

    Created the InventoryItem model

--- inventorymanagement/models.py
+++ inventorymanagement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+from authentication.models import User
+
+class InventoryItem(models.Model):
+
+    class Units(models.TextChoices):
+        NIL = 'nil', ('-')
+        GM = 'gm', ('gm')
+        KG = 'kg', ('Kg')
+        ML = 'ml', ('ml')
+        L = 'l', ('l')
+
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=3)
+    unit = models.CharField(max_length=2, choices=Units.choices, default=Units.NIL)
+
+    def __str__(self) -> str:
+        return self.name
